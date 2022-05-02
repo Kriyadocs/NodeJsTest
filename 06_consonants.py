95e28d296328f19b789787367251d9d39b8f2e9b
shiny-suresh <104665472+shiny-suresh@users.noreply.github.com>
2022-05-02 16:01:21 +0530

    Initial commit

---
+++ 06_consonants.py
@@ -0,0 +1,11 @@
+'''
+Write a program that print the frequency of all consonants (not vowels) in a string. 
+The output should be printed in alphabetical order of letters
+
+Sample Input:
+hello
+
+Sample Output:
+h 1
+l 2
+'''
